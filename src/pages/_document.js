11b840e8b63515c06c31762833706e06d89e4832
João Pedro Albuquerque <joaopedroalb.dev@gmail.com>
2022-05-06 15:14:43 -0300

    feat: config babel and create _document to handle with earlier render

---
+++ src/pages/_document.js
@@ -0,0 +1,21 @@
+import Document, { Head, Main, NextScript, Html } from 'next/document'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    // Returns an object like: { html, head, errorHtml, chunks, styles }     
+    return renderPage();
+  }
+
+  render () {    
+    return (
+      <Html>
+        <Head title='Aulaki'>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
